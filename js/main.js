a5ae72c35c6c9f3fb00c366eb0a2dd1007156cc5
org123-456 <1613764019@qq.com>
2026-09-09 14:01:23 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+import { PhoneUI } from './phone/phone_ui.js';
+import { PhoneEngine } from './phone/phone_engine.js';
+
+window.PhoneUI = PhoneUI;
+window.PhoneEngine = PhoneEngine;
+
+document.addEventListener('DOMContentLoaded', () => {
+    PhoneUI.init();
+});
 

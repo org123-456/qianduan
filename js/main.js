fbc8726d06dbd1dc10f0685f83680e192ae252bf
org123-456 <1613764019@qq.com>
2026-09-09 23:23:09 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import { PhoneUI } from './phone/phone_ui.js';
 import { PhoneEngine } from './phone/phone_engine.js';
+import { Config } from './phone/phone_config.js';
 
+// 把核心方法挂载到全局，防止 HTML 里的点击和滑动事件找不到它们
 window.PhoneUI = PhoneUI;
 window.PhoneEngine = PhoneEngine;
+window.Config = Config; 
 
 document.addEventListener('DOMContentLoaded', () => {
     PhoneUI.init();
 });
-

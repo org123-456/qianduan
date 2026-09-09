4cd84c6f564b6bd1d54daeef0d63c375f5b566b1
org123-456 <1613764019@qq.com>
2026-09-09 14:03:21 +0800

    Update phone_config.js

--- js/phone/phone_config.js
+++ js/phone/phone_config.js
@@ -1,1 +1,20 @@
+export const Config = {
+    currentContactId: null,
+    currentAppId: null,
+    externalData: {
+        "role_001": { 
+            name: "林萧", 
+            persona: "25岁，自由摄影师，性格随和，喜欢吃辣。", 
+            memory: "昨天刚发了工资，打算今天去商场买个新镜头，顺便吃顿火锅。",
+            recentChat: "用户：你今天出门了吗？\n林萧：刚出门，在地铁上了，准备去万达广场。"
+        },
+        "role_002": {
+            name: "霸总",
+            persona: "30岁，集团CEO，冷酷无情，只喝黑咖啡。",
+            memory: "今天下午3点有一个跨国并购会议。",
+            recentChat: "用户：老板，会议资料放您桌上了。\n霸总：知道了，别让任何人打扰我。"
+        }
+    },
+    phoneData: {} 
+};
 

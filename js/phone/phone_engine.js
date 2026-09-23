37f9ddd0a4bf67672f03abd23595b0d0596b465f
org123-456 <1613764019@qq.com>
2026-09-23 22:59:00 +0800

    Update phone_engine.js

--- js/phone/phone_engine.js
+++ js/phone/phone_engine.js
@@ -1,9 +1,13 @@
 import { ChatEngine } from './engine/chat_engine.js';
 import { ReaderEngine } from './engine/reader_engine.js';
+import { MemoryEngine } from './engine/memory_engine.js';
+import { GalleryEngine } from './engine/gallery_engine.js';
 
 export const PhoneEngine = {
   ...ChatEngine,
   ...ReaderEngine,
+  ...MemoryEngine,
+  ...GalleryEngine,
 };
 
 export default PhoneEngine;
